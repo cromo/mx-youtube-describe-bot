39198498dd465e2014b32615b10c8fc742d49052
Cristián Romo <cristian.g.romo@gmail.com>
2019-11-06 13:53:45 -0600

    Add rudimentary bot functionality
    It can get the video ID from a YouTube link, get some metadata for it,
    and send that back to chat as JSON.
    
    As a side note, the typings for Ramda don't include R.then, meaning that
    I had to split out my main response function some in order to handle
    the promises from node-fetch.

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import { readFileSync } from "fs";
 import * as TOML from "@iarna/toml";
 import * as R from "ramda";
+import * as fetch from "node-fetch";
 import { MatrixClient, AutojoinRoomsMixin, SimpleFsStorageProvider } from "matrix-bot-sdk";
 
 interface Config {
     homeserverUrl: string;
     accessToken: string;
     syncStateFile: string;
+    youTubeApiKey: string;
 }
 
 const readUtf8File = R.curryN(2, readFileSync)(R.__, {encoding: "utf-8"});
@@ -19,24 +21,32 @@ const hasContent = R.compose(R.not, R.isNil, R.prop("content"));
 const eventBody = R.path(["content", "body"]);
 const eventType = R.path(["content", "msgtype"]);
 const isTextMessage = R.pipe(eventType, R.equals("m.text"));
-const youTubeLink = R.pipe(eventBody, R.tap(console.log), R.match(/youtube\.com\/watch\?v=([a-zA-Z0-9_-]{11})/), R.tap(console.log));
-const hasYouTubeLink = R.pipe(youTubeLink, R.length, R.tap(console.log), R.lt(0), R.tap(console.log));
-//https://www.youtube.com/watch?v=TeG4XHxb6BU
+const youTubeLink = R.pipe(eventBody, R.match(/youtube\.com\/watch\?v=([a-zA-Z0-9_-]{11})/));
+const hasYouTubeLink = R.pipe(youTubeLink, R.length, R.lt(0));
 const shouldRespond = R.allPass([hasContent, isTextMessage, hasYouTubeLink]);
 const sendNotice = R.curry((client, roomId, message) => client.sendNotice(roomId, message));
 
+const youTubeVideo = R.curry((apiKey, videoId) => fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet,contentDetails&fields=items(id,snippet/title,contentDetails/duration)`).then(res => res.json()));
+
+
 const config = readConfig("config.toml") as unknown as Config;
 const client = getClient(config.homeserverUrl, config.accessToken, new SimpleFsStorageProvider(config.syncStateFile));
+const video = youTubeVideo(config.youTubeApiKey);
 
 client.start().then(() => console.log("mx-youtube-describe-bot connected"));
-client.on("room.message", (roomId, event) => {
+client.on("room.message", async (roomId, event) => {
+    if (!shouldRespond(event)) {
+        return;
+    }
     const notifyRoom = sendNotice(client, roomId);
-    R.when(
-        shouldRespond,
-        R.pipe(
-            eventBody,
-            notifyRoom
-        ),
-        event
-    );
+    const videoInfo = await R.pipe(
+        youTubeLink,
+        R.path([1]),
+        video,
+    )(event);
+    R.pipe(
+        R.path(["items", 0]),
+        R.toString,
+        notifyRoom
+    )(videoInfo);
 });
